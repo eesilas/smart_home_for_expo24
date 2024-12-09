7b2e9e5907b00eabe02c573fed95cccafa574731
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-12-09 14:55:25 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,33 @@
 let rain = 0
+let temp = 0
 xiamiBoard.initXiaMiBoard()
 I2C_LCD1602.LcdInit(39)
 I2C_LCD1602.BacklightOff()
 basic.pause(2000)
 I2C_LCD1602.BacklightOn()
+xiamiBoard.OLEDshowUserText("Silicon", 0, 0)
+xiamiBoard.OLEDshowUserText("Workshop", 2, 0)
+I2C_LCD1602.ShowString("Temperature", 0, 0)
+I2C_LCD1602.ShowString("Rain: ", 0, 2)
+basic.pause(2000)
 pins.servoWritePin(AnalogPin.P8, 180)
 basic.pause(1000)
 pins.servoWritePin(AnalogPin.P8, 90)
 basic.pause(1000)
 pins.servoWritePin(AnalogPin.P8, 0)
 basic.forever(function () {
-    I2C_LCD1602.ShowString("Smart Home", 0, 0)
-    I2C_LCD1602.ShowString("Rain: ", 0, 2)
-    I2C_LCD1602.ShowNumber(pins.analogReadPin(AnalogReadWritePin.P2), 8, 2)
-    basic.pause(2000)
-    xiamiBoard.OLEDshowUserText("Silicon", 0, 0)
-    xiamiBoard.OLEDshowUserText("Workshop", 2, 0)
-    if (pins.analogReadPin(AnalogReadWritePin.P2) >= 3600) {
-        pins.servoWritePin(AnalogPin.P8, 180)
+    rain = pins.analogReadPin(AnalogReadWritePin.P2)
+    temp = input.temperature()
+    I2C_LCD1602.ShowNumber(temp, 12, 0)
+    I2C_LCD1602.ShowNumber(rain, 8, 2)
+    if (rain >= 800) {
+        pins.servoWritePin(AnalogPin.P8, 90)
     } else {
         pins.servoWritePin(AnalogPin.P8, 0)
+    }
+    if (temp >= 26) {
+        xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CW, 128)
+    } else {
+        xiamiBoard.motorStop(MOTOR.M1)
     }
 })
